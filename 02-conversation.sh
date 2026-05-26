417908aa028f4b7d9bcf9d14bd0bebc56bf07861
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-26 12:10:42 +0100

    02-conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -6,4 +6,4 @@ PERSON2=Iran
 echo "$PERSON1: Hey Iran, we are going blast you!"
 echo "$PERSON2: Yes, please. you are welcome"
 echo "$PERSON1: Hey $PERSON2 I am serious"
-echo "$PERASON2: Hey $PERSON1, We are also serious"
+echo "$PERSON2: Hey $PERSON1, We are also serious"
