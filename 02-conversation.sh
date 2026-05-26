76d6fa854d07add247d5c0208b2044013efc1bd1
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-26 12:59:49 +0100

    03-variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Iran
+PERSON1=russia
+PERSON2=ukraine
 
-echo "$PERSON1: Hey Iran, we are going blast you!"
+echo "$PERSON1: Hey $PERSON2, we are going to blast you!"
 echo "$PERSON2: Yes, please. you are welcome"
 echo "$PERSON1: Hey $PERSON2 I am serious"
 echo "$PERSON2: Hey $PERSON1, We are also serious"
+
