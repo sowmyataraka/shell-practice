e3279adc7c370cdfac0167a961a2ef77899c4453
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-26 12:01:00 +0100

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Trump: Hey Iran, we are going blast you!"
-echo "Iran: Yes, please. you are welcome"
-echo "Trump : Hey Iran I am serious"
-echo "Iran Trump ,we are also serious"
+PERSON1=Trump
+PERSON2=Iran
+
+echo "$PERSON1: Hey Iran, we are going blast you!"
+echo "$PERSON2: Yes, please. you are welcome"
+echo "$PERSON1: Hey $PERSON2 I am serious"
+echo "$PERASON2: Hey $PERSON1, We are also serious"
