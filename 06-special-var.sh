c3f91b1a75f5018f11a5e408cd4320eb24ee891c
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-26 18:46:48 +0100

    06-special-var

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -5,8 +5,8 @@ echo "All variables passed to script:$@"
 echo "Number of variable passed: $|#"
 echo "First variable: $1"
 echo "script name: $0"
-echo"who is running this: $USER"
-
+echo "ho is running this: $USER"
+echo "which directory: $PWD" 
 
 
  
