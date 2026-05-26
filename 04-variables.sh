b1d9c44e075284d2fefe86855a3c51205dc8024c
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-26 13:20:34 +0100

    04-variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,3 +4,12 @@ echo "please enter username::"
 read USER_NAME
 
 echo "Username is $USER_NAME"
+
+echo "please enter your password::
+read PASSWORD
+
+echo "password is: "PASSWORD::"
+read -s PASSWORD
+
+echo "password is: $PASSWORD"
+
