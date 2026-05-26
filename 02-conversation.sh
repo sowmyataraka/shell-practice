fdbe9b324d208fc2d6c5b3c3b526c6c9b3093716
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-26 11:24:00 +0100

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Iran, we are going blast you!"
+echo "Iran: Yes, please. you are welcome"
+echo "Trump : Hey Iran I am serious"
+echo "Iran Trump ,we are also serious"
