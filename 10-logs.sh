cb6125b3e3f4b6638fde604ac350af4e7dc69372
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-27 18:40:53 +0100

    10-logs.sh

--- 10-logs.sh
+++ 10-logs.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOG_DIR=/home/ec2-user/shell-logs
-LOGS_FILE=$LOG_DIR/$(date +%F-%H-%M-%S).log
-# check root access or not
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
+
+# Check root access or not
 if [ $USERID -ne 0 ]; then
-        echo "Please run this scrippt with root access"
-        exit 1
- fi
+    echo "Please run this script with root access"
+    exit 1
+fi
 
 # first arg -> what are you trying to install
-# second arg ->exit copde
+# second arg -> exit code
 VALIDATE(){
-        if [ $2 -ne 0 ]; then
-                echo "Installing $1 is ...FAILED"
-                exit 1
-        else
-                echo "$1 is installed successfully"
-        fi
-}
-#echo "I am continuing .."
-dnf list installed mysql
-
-if [ $? -eq 0 ]; then 
-        echo "MySQL is already installed ...SKIPPING"
-else
-   echo "Installing MySQL"
-   dnf install mysql -y
-   VALIDATE MYSQL $? 
-   if 
-   
-   dnf list installed nginx
-   if [ $? -ne 0 ]; then 
-        echo "nginx is already installed ..SKIPPING"
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED" | tee -a $LOGS_FILE
         exit 1
-   else
-        echo "Installing nginx"
-        dnf install nginx -y
-        VALIDATE MYASQL $?
-  fi
+    else
+        echo "Installing $1 is ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
 
+# echo "I am continuing..."
+dnf list installed mysql &>> $LOGS_FILE
+
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING" | tee -a $LOGS_FILE
+else
+    echo "Installing MySQL"
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySQL $?
+fi
+
+dnf list installed nginx &>> $LOGS_FILE
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING" | tee -a $LOGS_FILE
+else
+    echo "Installing nginx"
+    dnf install nginx -y &>> $LOGS_FILE
+    VALIDATE MySQL $?
+fi
