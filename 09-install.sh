d4c977915884c2a05efd8f5155eba0ff90dd875c
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-27 20:45:02 +0100

    api doc added

--- 09-install.sh
+++ 09-install.sh
@@ -2,30 +2,39 @@
 
 USERID=$(id -u)
 
-# check root access or not
+# Check root access or not
 if [ $USERID -ne 0 ]; then
-        echo "Please run this scrippt with root access"
-        exit 1
- fi
+    echo "Please run this script with root access"
+    exit 1
+fi
 
-#echo "I am continuing .."
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
+
+# echo "I am continuing..."
 dnf list installed mysql
 
-if [ $? -eq 0 ]; then 
-        echo "MySQL is already installed ...SKIPPING"
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING"
 else
-   echo "Installing MySQL"
-   dnf install mysql -y
-   VALIDATE MYSQL $? 
-   if 
-   
-   dnf list installed nginx
-   if [ $? -ne 0 ]; then 
-        echo "nginx is already installed ..SKIPPING"
-        exit 1
-   else
-        echo "Installing nginx"
-        dnf install nginx -y
-        VALIDATE MYASQL $?
-  fi
+    echo "Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?
+fi
 
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING"
+else
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $?
+fi
---
+++ 12-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
+
+# Check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
+
+
+for package in $0
+do
+   echo "Installing $package"
+done
+
+
+
+
