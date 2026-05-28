9b66321ff4075c44df4dec520b674fc1b1a9bd8f
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-28 08:43:23 +0100

    13-colurs.sh

--- 15-set.sh
+++ 15-set.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+ #!/bin/bash
+set -e
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
@@ -15,16 +15,6 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-# first arg -> what are you trying to install
-# second arg -> exit code
-VALIDATE(){
-    if [ $2 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] Installing $1 is ...$R FAILED $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo "$TIMESTAMP [INFO] Installing $1 is ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    fi
-}
 
 
 for package in $@
